4d8ae91948319c7931f3f0c2abe3b474d2b15242
apoorvak1997 <45688556+apoorvak1997@users.noreply.github.com>
2021-12-09 18:20:14 -0500

    first commit

---
+++ neu-marketplace-front/marketplace-front/src/core/Menu.js
@@ -0,0 +1,118 @@
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth";
+import { itemTotal } from "./cartHelpers";
+
+const isActive = (history, path) => {
+    if (history.location.pathname === path) {
+        return { color: "#FF0000" };
+    } else {
+        return { color: "#ffffff" };
+    }
+};
+
+const Menu = ({ history }) => (
+    <div>
+        <ul className="nav nav-tabs bg-primary">
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/")}
+                    to="/"
+                >
+                    HOME
+                </Link>
+            </li>
+
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/shop")}
+                    to="/shop"
+                >
+                    SHOP
+                </Link>
+            </li>
+
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/cart")}
+                    to="/cart"
+                >
+                    CART{" "}<span><i class="fas fa-shopping-basket"></i></span>
+
+                    <sup>
+                        <small className="cart-badge">{itemTotal()}</small>
+                    </sup>
+                </Link>
+            </li>
+
+            {isAuthenticated() && isAuthenticated().user.role === 0 && (
+                <li className="nav-item">
+                    <Link
+                        className="nav-link"
+                        style={isActive(history, "/user/dashboard")}
+                        to="/user/dashboard"
+                    >
+                        DASHBOARD
+                    </Link>
+                </li>
+            )}
+
+            {isAuthenticated() && isAuthenticated().user.role === 1 && (
+                <li className="nav-item">
+                    <Link
+                        className="nav-link"
+                        style={isActive(history, "/admin/dashboard")}
+                        to="/admin/dashboard"
+                    >
+                        DASHBOARD
+                    </Link>
+                </li>
+            )}
+
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signin")}
+                            to="/signin"
+                        >
+                            SIGN IN
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signup")}
+                            to="/signup"
+                        >
+                            SIGN UP
+                        </Link>
+                    </li>
+                </Fragment>
+            )}
+
+            {isAuthenticated() && (
+                <li className="nav-item">
+                    <span
+                        className="nav-link"
+                        style={{ cursor: "pointer", color: "#ffffff" }}
+                        onClick={() =>
+                            signout(() => {
+                                history.push("/");
+                            })
+                        }
+                    >
+                        Signout
+                    </span>
+                </li>
+            )}
+        </ul>
+    </div>
+);
+
+export default withRouter(Menu);
---
+++ neu-marketplace-front/marketplace-front/src/user/Profile.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '../core/Layout';
+import { isAuthenticated } from '../auth';
+import { Link, Redirect } from 'react-router-dom';
+import { read, update, updateUser } from './apiUser';
+
+const Profile = ({ match }) => {
+    const [values, setValues] = useState({
+        name: '',
+        nuid: '',
+        email: '',
+        password: '',
+        error: false,
+        success: false
+    });
+
+    const { token } = isAuthenticated();
+    const { name, nuid, email, password, error, success } = values;
+
+    const init = userId => {
+        // console.log(userId);
+        read(userId, token).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: true });
+            } else {
+                setValues({ ...values, name: data.name, email: data.email, nuid: data.nuid});
+            }
+        });
+    };
+
+    useEffect(() => {
+        init(match.params.userId);
+    }, []);
+
+    const handleChange = name => e => {
+        setValues({ ...values, error: false, [name]: e.target.value });
+    };
+
+    const clickSubmit = e => {
+        e.preventDefault();
+        update(match.params.userId, token, { name, nuid, email, password }).then(data => {
+            if (data.error) {
+                // console.log(data.error);
+                alert(data.error);
+            } else {
+                updateUser(data, () => {
+                    setValues({
+                        ...values,
+                        name: data.name,
+                        email: data.email,
+                        nuid: data.nuid,
+                        success: true
+                    });
+                });
+            }
+        });
+    };
+
+    const redirectUser = success => {
+        if (success) {
+            return <Redirect to="/cart" />;
+        }
+    };
+
+    const profileUpdate = (name, nuid, email, password) => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input type="text" onChange={handleChange('name')} className="form-control" value={name} />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">NUID</label>
+                <input type="text" onChange={handleChange('nuid')} className="form-control" value={nuid} />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input type="email" onChange={handleChange('email')} className="form-control" value={email} />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input type="password" onChange={handleChange('password')} className="form-control" value={password} />
+            </div>
+
+            <button onClick={clickSubmit} className="btn btn-primary">
+                Submit
+            </button>
+        </form>
+    );
+
+    return (
+        <Layout title="Profile" description="Update your profile" className="container-fluid">
+            <h2 className="mb-4">Profile update</h2>
+            {profileUpdate(name, nuid, email, password)}
+            {redirectUser(success)}
+        </Layout>
+    );
+};
+
+export default Profile;
